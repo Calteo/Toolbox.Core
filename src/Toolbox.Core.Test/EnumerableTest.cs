e3f63890f33aab76910e6adb84744e1ab10f8098
Calteo <matthias@famger.de>
2020-12-22 19:11:22 +0100

    added ForEach extension in IEnumerable<T>

---
+++ src/Toolbox.Core.Test/EnumerableTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Toolbox.Collection.Generics;
+
+namespace Toolbox.Core.Test
+{
+    [TestClass]
+    public class EnumerableTest
+    {
+        [TestMethod]
+        public void ForEachAction()
+        {
+            var cut = new int[20];
+            var random = new Random();
+
+            for (int i = 0; i < cut.Length; i++)
+            {
+                cut[i] = random.Next();
+            }
+
+            var results = new List<int>();
+
+            cut.ForEach(i => results.Add(i));
+
+            Assert.AreEqual(cut.Length, results.Count);
+            for (var i = 0; i < cut.Length; i++)
+            {
+                Assert.AreEqual(cut[i], results[i], $"item[{i}]");
+            }            
+        }
+    }
+}
---
+++ src/Toolbox.Core/Collection/Generics/EnumerableExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toolbox.Collection.Generics
+{
+    public static class EnumerableExtension
+    {
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var item in collection)
+            {
+                action(item);
+            }
+        }
+    }
+}
