2259ea96bc317cbfa5d1149d5fded4e5e2050a14
Calteo <matthias@famger.de>
2024-01-08 21:39:47 +0100

    Added InheritableBoolean

---
+++ src/Toolbox.Core.Test/InheritableBooleanTest.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toolbox.Core.Test
+{
+    [TestClass]
+    public class InheritableBooleanTest
+    {
+        [TestMethod]
+        public void ImplicitConvertTrue()
+        {
+            InheritableBoolean cut = true;
+
+            Assert.AreSame(InheritableBoolean.True, cut);
+        }
+
+        [TestMethod]
+        public void ImplicitConvertFalse()
+        {
+            InheritableBoolean cut = false;
+
+            Assert.AreSame(InheritableBoolean.False, cut);
+        }
+
+        [TestMethod]
+        public void EqualsTrueTrue()
+        {
+            InheritableBoolean arg1 = true;
+            InheritableBoolean arg2 = true;
+
+            var cut = arg1 == arg2;
+
+            Assert.IsTrue(cut);
+        }
+
+        [TestMethod]
+        public void EqualsTrueFalse()
+        {
+            InheritableBoolean arg1 = true;
+            InheritableBoolean arg2 = false;
+
+            var cut = arg1 == arg2;
+
+            Assert.IsFalse(cut);
+        }
+
+        [TestMethod]
+        public void EqualsDefaultDefault()
+        {
+            var arg1 = InheritableBoolean.Default;
+            var arg2 = InheritableBoolean.Default;
+
+            var cut = arg1 == arg2;
+
+            Assert.IsTrue(cut);
+        }
+
+        [TestMethod]
+        public void EqualsDefaultTrue()
+        {
+            var arg1 = InheritableBoolean.Default;
+            var arg2 = InheritableBoolean.True;
+
+            var cut = arg1 == arg2;
+
+            Assert.IsFalse(cut);
+        }
+
+        [TestMethod]
+        public void AndTrueTrue()
+        {
+            var arg1 = InheritableBoolean.True;
+            var arg2 = InheritableBoolean.True;
+
+            var cut = arg1 && arg2;
+
+            Assert.IsTrue((bool)cut);
+        }
+        [TestMethod]
+        public void AndTrueDefault()
+        {
+            var arg1 = InheritableBoolean.True;
+            var arg2 = InheritableBoolean.Default;
+
+            var cut = arg1 && arg2;
+
+            Assert.IsTrue((bool)cut);
+        }
+        [TestMethod]
+        public void AndTrueFalse()
+        {
+            var arg1 = InheritableBoolean.True;
+            var arg2 = InheritableBoolean.False;
+
+            var cut = arg1 && arg2;
+
+            Assert.IsFalse((bool)cut);
+        }
+        [TestMethod]
+        public void AndFalseDefault()
+        {
+            var arg1 = InheritableBoolean.False;
+            var arg2 = InheritableBoolean.False;
+
+            var cut = arg1 && arg2;
+
+            Assert.IsFalse((bool)cut);
+        }
+        [TestMethod]
+        public void OrTrueTrue()
+        {
+            var arg1 = InheritableBoolean.True;
+            var arg2 = InheritableBoolean.True;
+
+            var cut = arg1 || arg2;
+
+            Assert.IsTrue((bool)cut);
+        }
+        [TestMethod]
+        public void OrTrueFalse()
+        {
+            var arg1 = InheritableBoolean.True;
+            var arg2 = InheritableBoolean.False;
+
+            var cut = arg1 || arg2;
+
+            Assert.IsTrue((bool)cut);
+        }
+        [TestMethod]
+        public void OrFalseFalse()
+        {
+            var arg1 = InheritableBoolean.False;
+            var arg2 = InheritableBoolean.False;
+
+            var cut = arg1 || arg2;
+
+            Assert.IsFalse((bool)cut);
+        }
+
+        [TestMethod]
+        public void InheritDefaultTrue()
+        {
+            var cut = InheritableBoolean.Default;
+            var arg = InheritableBoolean.True;
+
+            var result = cut.Inherit(arg);
+
+            Assert.IsTrue((bool)result);
+        }
+        [TestMethod]
+        public void InheritDefaultFalse()
+        {
+            var cut = InheritableBoolean.Default;
+            var arg = InheritableBoolean.False;
+
+            var result = cut.Inherit(arg);
+
+            Assert.IsFalse((bool)result);
+        }
+        [TestMethod]
+        public void InheritDefaultDefault()
+        {
+            var cut = InheritableBoolean.Default;
+            var arg = InheritableBoolean.Default;
+
+            var result = cut.Inherit(arg);
+
+            Assert.AreSame(InheritableBoolean.Default, result);
+        }
+        [TestMethod]
+        public void InheritTrueDefault()
+        {
+            var cut = InheritableBoolean.True;
+            var arg = InheritableBoolean.Default;
+
+            var result = cut.Inherit(arg);
+
+            Assert.IsTrue((bool)result);
+        }
+        [TestMethod]
+        public void InheritFalseDefault()
+        {
+            var cut = InheritableBoolean.False;
+            var arg = InheritableBoolean.Default;
+
+            var result = cut.Inherit(arg);
+
+            Assert.IsFalse((bool)result);
+        }
+    }
+}
---
+++ src/Toolbox.Core/InheritableBoolean.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox
+{
+    /// <summary>
+    /// Calls to make <see cref="bool"/> inheriable values.
+    /// </summary>
+    public class InheritableBoolean
+    {
+        public static InheritableBoolean True { get; } = new InheritableBoolean();
+        public static InheritableBoolean False { get; } = new InheritableBoolean();
+        public static InheritableBoolean Default { get; } = new InheritableBoolean();
+
+        public static implicit operator InheritableBoolean(bool value) => value ? True : False;
+        public static explicit operator bool(InheritableBoolean value)
+        {
+            if (value == null) throw new ArgumentNullException();
+            if (value == Default) throw new InvalidCastException("Default can not be converted.");
+
+            return value == True;
+        }
+
+        public static bool operator true(InheritableBoolean value) => value == True;
+        public static bool operator false(InheritableBoolean value) => value == False;
+
+        public static InheritableBoolean operator &(InheritableBoolean value1, InheritableBoolean value2)
+        {
+            if (value1 == Default) return value2;
+            if (value2 == Default) return value1;
+
+            if (value1==False || value2==False) return False;
+            return True;
+        }
+
+        public static InheritableBoolean operator |(InheritableBoolean value1, InheritableBoolean value2)
+        {
+            if (value1 == Default) return value2;
+            if (value2 == Default) return value1;
+
+            if (value1 == True || value2 == True) return True;
+            return False;
+        }
+
+        public InheritableBoolean Inherit(InheritableBoolean value)
+        {
+            if (this != Default) return this;
+
+            return value;
+        }
+    }
+}
