7a4668594ffa7667fb21cf2d5668805c3b8994c9
Calteo <matthias@famger.de>
2024-11-09 21:01:08 +0100

    added extensions for type class

---
+++ src/Toolbox.Core.Test/TypeExtensionTest.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toolbox.Core.Test.Model;
+
+namespace Toolbox.Core.Test
+{
+	[TestClass]
+	public class TypeExtensionTest
+	{
+		[TestMethod]
+		public void GetNameSimpleClass()
+		{
+			var type = typeof(Data);
+
+			var result = type.GetTypeName();
+
+			Assert.AreEqual(type.Name, result);
+		}
+
+		[TestMethod]
+		public void GetNameSimpleClassWithNamespace()
+		{
+			var type = typeof(Data);
+
+			var result = type.GetTypeName(true);
+
+			Assert.AreEqual(type.Namespace + "." + type.Name, result);
+		}
+
+		[TestMethod]
+		public void GetNameGenericClass()
+		{
+			var type = typeof(List<string>);
+
+			var result = type.GetTypeName();
+
+			Assert.AreEqual("List<String>", result);
+		}
+
+		[TestMethod]
+		public void GetNameGenericClassWithNamespace()
+		{
+			var type = typeof(List<string>);
+
+			var result = type.GetTypeName(true);
+
+			Assert.AreEqual("System.Collections.Generic.List<System.String>", result);
+		}
+
+		[TestMethod]
+		public void GetNameDictionaryClass()
+		{
+			var type = typeof(Dictionary<int, Data>);
+
+			var result = type.GetTypeName();
+
+			Assert.AreEqual("Dictionary<Int32,Data>", result);
+		}
+
+		[TestMethod]
+		public void GetNameDictionaryClassWithNamespace()
+		{
+			var type = typeof(Dictionary<int, Data>);
+
+			var result = type.GetTypeName(true);
+
+			Assert.AreEqual("System.Collections.Generic.Dictionary<System.Int32,Toolbox.Core.Test.Model.Data>", result);
+		}
+
+		class Nested
+		{
+		}
+
+		class NestedGeneric<T>
+		{
+		}
+
+
+		[TestMethod]
+		public void GetNameNestedClass()
+		{
+			var type = typeof(Nested);
+
+			var result = type.GetTypeName();
+
+			Assert.AreEqual("TypeExtensionTest+Nested", result);
+		}
+
+		[TestMethod]
+		public void GetNameNestedClassWithNamespace()
+		{
+			var type = typeof(Nested);
+
+			var result = type.GetTypeName(true);
+
+			Assert.AreEqual("Toolbox.Core.Test.TypeExtensionTest+Nested", result);
+		}
+
+		[TestMethod]
+		public void GetNameNestedGenericClass()
+		{
+			var type = typeof(NestedGeneric<bool>);
+
+			var result = type.GetTypeName();
+
+			Assert.AreEqual("TypeExtensionTest+NestedGeneric<Boolean>", result);
+		}
+
+		[TestMethod]
+		public void GetNameNestedGenericClassWithNamespace()
+		{
+			var type = typeof(NestedGeneric<bool>);
+
+			var result = type.GetTypeName(true);
+
+			Assert.AreEqual("Toolbox.Core.Test.TypeExtensionTest+NestedGeneric<System.Boolean>", result);
+		}
+
+		[TestMethod]
+		public void GetNameGenericTemplate()
+		{
+			var type = typeof(HashSet<>);
+
+			var result = type.GetTypeName();
+
+			Assert.AreEqual("HashSet<T>", result);
+		}
+
+		[TestMethod]
+		public void GetNameGenericTempalteWithNamespace()
+		{
+			var type = typeof(HashSet<>);
+
+			var result = type.GetTypeName(true);
+
+			Assert.AreEqual("System.Collections.Generic.HashSet<T>", result);
+		}
+
+
+	}
+}
---
+++ src/Toolbox.Core/TypeExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Toolbox
+{
+	/// <summary>
+	/// Extensions to the <see cref="Type"/> class.
+	/// </summary>
+	public static class TypeExtension
+	{
+		/// <summary>
+		/// Get the name for the type with generic parameters resolved.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		public static string GetTypeName(this Type type, bool includeNamespace = false)
+		{
+			if (!type.IsGenericType)
+			{
+				if (!type.IsNested)
+					return $"{(includeNamespace ? type.Namespace + "." : "")}{type.Name}";
+				else
+					return $"{type.DeclaringType.GetTypeName(includeNamespace)}+{type.Name}";
+			}
+
+			var shortName = type.Name.Split('`')[0];
+
+
+            var arguments = type.IsConstructedGenericType 
+								? type.GetGenericArguments().Select(a => a.GetTypeName(includeNamespace))
+								: type.GetGenericTypeDefinition().GetGenericArguments().Select(a => a.Name);
+
+			if (!type.IsNested)
+				return $"{(includeNamespace ? type.Namespace + "." : "")}{shortName}<{string.Join(",", arguments)}>";
+
+			return $"{type.DeclaringType.GetTypeName(includeNamespace)}+{shortName}<{string.Join(",", arguments)}>";
+		}
+	}
+}
